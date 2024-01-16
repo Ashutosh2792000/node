44ef4c041ec5d864ea01fb91ea1f99867876adbf
Ashutosh kumar raj <122087921+Ashutosh2792000@users.noreply.github.com>
2024-01-16 16:07:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+const { request } = require('node:https')
+
 http = require('node:http')
-server = http.createServer((req,res)=>{
-    data = {name:'ashutosh',location:'Patna',age:23}
+fs = require('node:fs')
+//server = http.createServer((req,res)=>{
+    // data = {name:'ashutosh',location:'Patna',age:23}
 
-    res.writeHead(200,{'content-type':'text/plain'})
-    res.end(JSON.stringify(data))
+    // res.writeHead(200,{'content-type':'text/plain'})
+    // res.end(JSON.stringify(data))
+
+    // html = fs.readFileSync('./index.html','utf-8')
+
+    // res.writeHead(200,{'content-type':'text/html'})
+    // res.end(req.url)
+
+    server = http.createServer((req,res)=>{
+        index = fs.readFileSync('./index.html','utf-8')
+        if(req.url==='/'){res.end(index)}
+    }
 
 
-})
+)
 
 server.listen(3000,()=>{console.log('success')})
