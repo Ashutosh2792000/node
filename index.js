fc9241136cc890e265337d21d1871c6b9b6adb30
Ashutosh kumar raj <122087921+Ashutosh2792000@users.noreply.github.com>
2024-01-16 15:05:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 http = require('node:http')
 server = http.createServer((req,res)=>{
+    data = {name:'ashutosh',location:'Patna',age:23}
 
     res.writeHead(200,{'content-type':'text/plain'})
-    res.end('This is Body')
+    res.end(JSON.stringify(data))
 
 
 })
